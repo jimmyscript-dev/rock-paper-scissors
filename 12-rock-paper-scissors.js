85773ea387061c3b43783f587cb23e1889988482
Jimrose S. Sarmiento <sarmientojimrose@gmail.com>
2023-06-10 19:28:08 +0800

    updated unreadable emoji code

--- 12-rock-paper-scissors.js
+++ 12-rock-paper-scissors.js
@@ -74,7 +74,7 @@ function pickComputerMove() {
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = '✊';
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = '🖐';
+    computerMove = '✋';
   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
     computerMove = '✌';
   }
